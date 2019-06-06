99a33ecd1c7ebb9f5214542d4c4f19d2452e4008
linusaurus <richard@weaselware.biz>
2019-06-06 12:44:26 -0700

    Switch Added and Status State finalized

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,6 @@
 *  UnitName = DS-MSTR-CTRL
 *  Controls 2 SLAVE TWIN-MOTOR CONTROLLERs
 ***********************************************************************************/
-
 #include <Arduino.h>
 #include <SPI.h>
 #include <Ethernet.h>
@@ -19,13 +18,12 @@
 // *******************************************************************************
 // Pins 
 // *******************************************************************************
- #define switchPin     2
- #define statusLedPin  3
-
+ #define masterSwitchPin     2
+ #define statusLedPin        3
 // Automaton Objects -------------------------------------------------------------
 Atm_led statusLED;
-// State Flags -------------------------------------------------------------------
-
+Atm_button masterSwitch;
+// State SYSTEM Flags -------------------------------------------------------------------
  bool SYSTEM_STOPPED{false};
  bool SYSTEM_CLOSED{false};
  bool SYSTEM_OPENED{false};
@@ -38,24 +36,28 @@ Atm_led statusLED;
  bool LF1_CLOSED{false};
  bool LF1_MOVING{false};
  bool LF1_ERROR{false};
+ bool LF1_ONLINE_OK{false};
 // LF2 ---------------------------------------------------------------------------
  bool LF2_STOPPED{false};
  bool LF2_OPEN{false};
  bool LF2_CLOSED{false};
  bool LF2_MOVING{false};
  bool LF2_ERROR{false};
+ bool LF2_ONLINE_OK{false};
 // LF3 ---------------------------------------------------------------------------
  bool LF3_STOPPED{false};
  bool LF3_OPEN{false};
  bool LF3_CLOSED{false};
  bool LF3_MOVING{false};
  bool LF3_ERROR{false};
+ bool LF3_ONLINE_OK{false};
 // LF4 ----------------------------------------------------------------------------
  bool LF4_STOPPED{false};
  bool LF4_OPEN{false};
  bool LF4_CLOSED{false};
  bool LF4_MOVING{false};
  bool LF4_ERROR{false};
+ bool LF4_ONLINE_OK{false};
 // -------------------------------------------------------------------------------
 byte mac[] = {
   0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
@@ -70,15 +72,28 @@ EthernetClient ethclient;
 PubSubClient client(ethclient);
 
 unsigned long previousMillis;
-unsigned long polling_interval = 2000;
+unsigned long polling_interval = 1000;
 int position = 0;
 
 void CheckState(){
 
+  if (LF1_ONLINE_OK && LF2_ONLINE_OK && LF3_ONLINE_OK && LF4_ONLINE_OK){
+    if(!SYSTEM_ONLINE_OK){
+    SYSTEM_ONLINE_OK=true;
+    SYSTEM_STOPPED=false;
+    SYSTEM_CLOSED=false;
+    SYSTEM_OPENED=false;
+    SYSTEM_IN_MOTION=false;
+    Serial.println("SYSTEM ONLINE OK");
+    }
+  }
   if (LF1_STOPPED && LF2_STOPPED && LF3_STOPPED && LF4_STOPPED)
   {
     if(!SYSTEM_STOPPED){
     SYSTEM_STOPPED=true;
+    SYSTEM_CLOSED=false;
+    SYSTEM_OPENED=false;
+    SYSTEM_IN_MOTION=false;
     Serial.println("SYSTEM STOPPED");
     }
 
@@ -89,6 +104,7 @@ void CheckState(){
       SYSTEM_CLOSED=true;
       SYSTEM_OPENED=false;
       SYSTEM_IN_MOTION=false;
+      SYSTEM_STOPPED=false;
       Serial.println("SYSTEM CLOSED");
     } 
   }
@@ -99,6 +115,7 @@ void CheckState(){
     SYSTEM_OPENED=true;
     SYSTEM_CLOSED=false;
     SYSTEM_IN_MOTION=false;
+    SYSTEM_STOPPED=false;
     Serial.println("SYSTEM OPENED");
     }
   }
@@ -111,16 +128,29 @@ void CheckState(){
     Serial.println("SYSTEM IN MOTION");
     }
   }
+
+   if (LF1_ERROR || LF2_ERROR || LF3_ERROR || LF4_ERROR){
+    if(!SYSTEM_ERROR_STATE){
+    SYSTEM_IN_MOTION=false;
+    SYSTEM_CLOSED=false;
+    SYSTEM_OPENED=false;
+    SYSTEM_STOPPED=false;
+    Serial.println("SYSTEM ERROR");
+    }
+  }
   
 }
 
-void callback(char* topic, byte* payload, unsigned int length) {
+void callback(char* topic, byte* payload, unsigned int length){
+
   Serial.print("Message arrived [");
   Serial.print(topic);
   Serial.print("] ");
+
   for (unsigned int i = 0; i < length; i++) {
     Serial.print((char)payload[i]);
   }
+
   
   Serial.println();
   /*************************************************
@@ -128,44 +158,48 @@ void callback(char* topic, byte* payload, unsigned int length) {
   **************************************************/
    if ((char)payload[0] == '1') {
    
-    if ((char)payload[1] == '1')
-    {
+    if ((char)payload[1] == '0'){
+     LF1_STOPPED=true;
+    }
+    if ((char)payload[1] == '1'){
      LF1_CLOSED=true;
     }
-    if ((char)payload[1] == '2')
-    {
+    if ((char)payload[1] == '2'){
      LF1_OPEN=true;
     }
-    if ((char)payload[1] == '3')
-    {
+    if((char)payload[1] == '3'){
      LF1_MOVING=true;
     }
-    if ((char)payload[1] == '4')
-    {
+    if ((char)payload[1] == '4'){
      LF1_ERROR=true;
+    }
+    if ((char)payload[1] == '5'){
+     LF1_ONLINE_OK=true;
     }
       
   } 
   /*************************************************
    * LF2 STATUS
   **************************************************/
-  if ((char)payload[0] == '2') {
+  if((char)payload[0] == '2') {
    
-    if ((char)payload[1] == '1')
-    {
+    if ((char)payload[1] == '0'){
+     LF2_STOPPED=true;
+    }
+    if ((char)payload[1] == '1'){
      LF2_CLOSED=true;
     }
-    if ((char)payload[1] == '2')
-    {
+    if ((char)payload[1] == '2'){
      LF2_OPEN=true;
     }
-    if ((char)payload[1] == '3')
-    {
+    if ((char)payload[1] == '3'){
      LF2_MOVING=true;
     }
-    if ((char)payload[1] == '4')
-    {
+    if ((char)payload[1] == '4'){
      LF2_ERROR=true;
+    }
+    if ((char)payload[1] == '5'){
+     LF2_ONLINE_OK=true;
     }
       
   } 
@@ -174,21 +208,23 @@ void callback(char* topic, byte* payload, unsigned int length) {
   **************************************************/
   if ((char)payload[0] == '3') {
    
-    if ((char)payload[1] == '1')
-    {
+    if ((char)payload[1] == '0'){
+     LF3_STOPPED=true;
+    }
+    if ((char)payload[1] == '1'){
      LF3_CLOSED=true;
     }
-    if ((char)payload[1] == '2')
-    {
+    if ((char)payload[1] == '2'){
      LF3_OPEN=true;
     }
-    if ((char)payload[1] == '3')
-    {
+    if ((char)payload[1] == '3'){
      LF3_MOVING=true;
     }
-    if ((char)payload[1] == '4')
-    {
+    if ((char)payload[1] == '4'){
      LF3_ERROR=true;
+    } 
+    if ((char)payload[1] == '5'){
+     LF3_ONLINE_OK=true;
     }  
   } 
 
@@ -197,24 +233,27 @@ void callback(char* topic, byte* payload, unsigned int length) {
   **************************************************/
   if ((char)payload[0] == '4') {
    
-    if ((char)payload[1] == '1')
-    {
+    if ((char)payload[1] == '0'){
+     LF4_STOPPED=true;
+    }
+    if ((char)payload[1] == '1'){
      LF4_CLOSED=true;
     }
-    if ((char)payload[1] == '2')
-    {
+    if ((char)payload[1] == '2'){
      LF4_OPEN=true;
     }
-    if ((char)payload[1] == '3')
-    {
+    if ((char)payload[1] == '3'){
      LF4_MOVING=true;
     }
-    if ((char)payload[1] == '4')
-    {
+    if ((char)payload[1] == '4'){
      LF4_ERROR=true;
+    }
+    if ((char)payload[1] == '5'){
+     LF4_ONLINE_OK=true;
     }
        
   } 
+
 }
 
 void reconnect() {
@@ -225,7 +264,7 @@ void reconnect() {
     if (client.connect("LFMSTR")) {
       Serial.println("connected");
       // Once connected, publish an announcement...
-      client.publish("STATUS", "CONTROLLER-ONLINE");
+      client.publish("SYS", "1");
       
       // ... and resubscribe
       client.subscribe("STATUS");
@@ -238,8 +277,26 @@ void reconnect() {
     }
   }
 }
- 
 
+void button_change( int idx, int v, int up ) {
+
+  Serial.println("Button Pressed -> CLOSE");
+  if (!SYSTEM_CLOSED){
+    client.publish("SIGNAL","1");
+  }
+  
+  
+}
+
+void button_release( int idx, int v, int up ) {
+
+  Serial.println("Button Release -> OPEN");
+  if(!SYSTEM_OPENED){
+  client.publish("SIGNAL","2");
+  }
+  
+}
+ 
 void setup() {
   
   Serial.begin(9600);
@@ -251,6 +308,9 @@ void setup() {
   client.setCallback(callback);
   Ethernet.begin(mac, ip);
   Serial.println(Ethernet.localIP());
+  masterSwitch.begin(masterSwitchPin);
+  masterSwitch.onPress(button_change);
+  masterSwitch.onRelease(button_release);
 }
 
 void loop() {
@@ -272,5 +332,3 @@ void loop() {
   }
 
 }
-
-
