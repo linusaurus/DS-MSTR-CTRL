8bd30d5074099ecf5298ba61d078700c5c3730d9
linusaurus <richard@weaselware.biz>
2019-06-19 16:14:55 -0700

    dhdh

--- src/main.cpp
+++ src/main.cpp
@@ -16,6 +16,27 @@
 #include <PubSubClient.h>
 #include <Automaton.h>
 #include <EEPROM.h>
+#include <LiquidCrystal_I2C.h>
+
+//(----------------------------------------------------------------------------------)
+
+#if defined(ARDUINO) && ARDUINO >= 100
+#define printByte(args)  write(args);
+#else
+#define printByte(args)  print(args,BYTE);
+#endif
+
+uint8_t bell[8]  = {0x4,0xe,0xe,0xe,0x1f,0x0,0x4};
+uint8_t note[8]  = {0x2,0x3,0x2,0xe,0x1e,0xc,0x0};
+uint8_t clock[8] = {0x0,0xe,0x15,0x17,0x11,0xe,0x0};
+uint8_t heart[8] = {0x0,0xa,0x1f,0x1f,0xe,0x4,0x0};
+uint8_t duck[8]  = {0x0,0xc,0x1d,0xf,0xf,0x6,0x0};
+uint8_t check[8] = {0x0,0x1,0x3,0x16,0x1c,0x8,0x0};
+uint8_t cross[8] = {0x0,0x1b,0xe,0x4,0xe,0x1b,0x0};
+uint8_t retarrow[8] = {	0x1,0x1,0x5,0x9,0x1f,0x8,0x4};
+
+LiquidCrystal_I2C lcd(0x27,20,4);  // set the LCD address to 0x27 for a 16 chars and 2 line display
+//(---------------------------------------------------------------------------------------------------)
 
 // *******************************************************************************
 // Pins 
@@ -81,11 +102,11 @@ PubSubClient client(ethclient);
 unsigned long previousMillis;
 unsigned long polling_interval = 1000;
 int position = 0;
-int command{0};
+int current_command{0};
 
 void CheckState(){
 
-  command=EEPROM.read(0);
+  current_command=EEPROM.read(0);
 
   if (LF1_ONLINE_OK && LF2_ONLINE_OK && LF3_ONLINE_OK && LF4_ONLINE_OK){
     if(!SYSTEM_ONLINE_OK){
@@ -106,7 +127,7 @@ void CheckState(){
     SYSTEM_IN_MOTION=false;
     Serial.println("SYSTEM STOPPED");
     downStatusLed.trigger(downStatusLed.EVT_ON);
-    EPProm.write(1,SYSTEM_STOPPED);
+    EEPROM.write(1,SYSTEM_STOPPED);
     }
 
   }
@@ -142,10 +163,10 @@ void CheckState(){
     SYSTEM_OPENED=false;
     SYSTEM_STOPPED=false;
     Serial.println("SYSTEM IN MOTION");
-    if(command==1){
+    if(current_command==1){
       downStatusLed.trigger(downStatusLed.EVT_BLINK  );
     }
-    else if(command==2){
+    else if(current_command==2){
       upStatusLed.trigger(upStatusLed.EVT_BLINK  );
     }
     }
@@ -290,6 +311,16 @@ void reconnect() {
       // Once connected, publish an announcement...
       client.publish("SYS", "1");
       connectionLED.trigger(connectionLED.EVT_ON);
+      lcd.setCursor(0,0);
+      lcd.print("Connection = OK");
+      char buffer[15];
+      sprintf(buffer,"current_command = %d ",current_command);
+       lcd.setCursor(0,1);
+     
+      lcd.print(buffer);
+      lcd.setCursor(0,2);
+
+      lcd.printByte(3);
       // ... and resubscribe
       client.subscribe("STATUS");
     } else {
@@ -305,11 +336,11 @@ void reconnect() {
 void button_change( int idx, int v, int up ) {
 
   
-  if (!SYSTEM_CLOSED || command==1){
+  if (!SYSTEM_CLOSED || current_command==1){
     Serial.println("Button Pressed -> CLOSE");
     client.publish("SIGNAL","1");
-    command=1;
-    EEPROM.write(0,command);
+    current_command=1;
+    EEPROM.write(0,current_command);
    
   }
   
@@ -319,12 +350,11 @@ void button_change( int idx, int v, int up ) {
 void button_release( int idx, int v, int up ) {
 
  
-  if(!SYSTEM_OPENED || command==2){
+  if(!SYSTEM_OPENED || current_command==2){
   Serial.println("Button Release -> OPEN");
   client.publish("SIGNAL","2");
-  command=2;
-  EEPROM.write(0,command);
- 
+  current_command=2;
+  EEPROM.write(0,current_command);
  
   }
   
@@ -347,10 +377,22 @@ void setup() {
   connectionLED.begin(connectionLedPin).blink(40,750);
   downStatusLed.begin(downStatusLedPin).blink(20,400);
   upStatusLed.begin(upStatusLedPin).blink(20,400);
+
+  lcd.init();                      // initialize the lcd 
+  lcd.backlight();
   
-  command = EEPROM.read(0);
-  Serial.print("Command = ");
-  Serial.println(command);
+  lcd.createChar(0, bell);
+  lcd.createChar(1, note);
+  lcd.createChar(2, clock);
+  lcd.createChar(3, heart);
+  lcd.createChar(4, duck);
+  lcd.createChar(5, check);
+  lcd.createChar(6, cross);
+  lcd.createChar(7, retarrow);
+  lcd.home();
+  
+  current_command = EEPROM.read(0);
+  
   
 }
 
